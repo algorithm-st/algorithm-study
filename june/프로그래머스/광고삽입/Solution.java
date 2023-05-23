6efeb56fd2b11174bb5f17dfac69c7dbdebdc0ba
june.sh22 <june.sh22@kakaocorp.com>
2023-05-23 22:17:20 +0900

    #90 <june> henry 풀이

--- june/프로그래머스/광고삽입/Solution.java
+++ june/프로그래머스/광고삽입/Solution.java
@@ -1,40 +1,47 @@
 package june.프로그래머스.광고삽입;
 
-class Solution {
+import java.util.*;
+
+public class Solution {
     public String solution(String play_time, String adv_time, String[] logs) {
-        String answer = "";
-        int playTime = convert(play_time);
-        int advTime = convert(adv_time);
-        int[] playSum = new int[100*60*60];
+        int playTime = convertSecond(play_time);
+        int advTime = convertSecond(adv_time);
+        int[] playCount = new int[100*60*60];
+        
         for(String log : logs){
-            String[] se = log.split("-");
-            int start = convert(se[0]);
-            int end = convert(se[1]);
-            for(int i = start; i<end; i++){
-                playSum[i]++;
+            String[] split = log.split("-");
+            int start = convertSecond(split[0]);
+            int end = convertSecond(split[1]);
+            for(int i = start; i< end; i++){
+                playCount[i]++;
             }
         }
         
         long sum = 0;
         for(int i =0; i<advTime; i++){
-            sum += playSum[i];
+            sum += playCount[i];
         }
         long max = sum;
         int index = 0;
-        for(int i = advTime; i < playTime; i++){
-            sum = sum - playSum[i - advTime] + playSum[i];
-            if(max < sum){
+        for(int i = advTime; i<playTime; i++){
+            sum = sum + playCount[i] - playCount[i - advTime];
+            if(sum > max){
                 max = sum;
                 index = i - advTime + 1;
             }
         }
         
-        return String.format("%02d:%02d:%02d", index/3600, (index/60)%60, index%60);
+        return String.format("%02d:%02d:%02d", index/3600, (index/60)%60,index%60);
     }
     
-    public int convert(String time){
-        String[] hms = time.split(":");
-        
-        return Integer.parseInt(hms[0])*60*60 + Integer.parseInt(hms[1])*60 + Integer.parseInt(hms[2]);
+    int convertSecond(String time){
+        int[] parse = Arrays.stream(time.split(":"))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+        return parse[0] * 60*60 + parse[1] * 60 + parse[2];
     }
 }
+
+// 시간을 초로 변환하는 함수
+// 변환하고 log 사이의 값들을 카운트 ++
+// 시작부터 제일 큰값 찾기 고고
