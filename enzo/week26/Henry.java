528bbd3de81638c80db444190d03c0114c6e845b
jhseo03 <jhseo03@cafe24corp.com>
2023-08-05 23:17:16 +0900

    #118 <enzo> henry 풀이

---
+++ enzo/week26/Henry.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        Queue<Integer> pqA = new PriorityQueue<>();
+        Queue<Integer> pqB = new PriorityQueue<>();
+        for(int i : A){
+            pqA.add(i);
+        }
+        for(int i : B){
+            pqB.add(i);
+        }
+        int answer = 0;
+        while(!pqA.isEmpty() && !pqB.isEmpty()){
+            int a = pqA.poll();
+            while(!pqB.isEmpty()){
+                int b = pqB.poll();
+                if(a<b){
+                    answer++;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
